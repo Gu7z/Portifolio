0ab8399074ac8ba9b4a989563b99aa5160834505
Gu7z <gustavoferri13@gmail.com>
2022-02-28 02:16:41 -0400

    add global app classname

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { StickyBar } from "./components";
 import "./App.css";
+import About from "./components/about";
 
 function App() {
+  // Render na stickybar so se for mobile
   return (
-    <>
+    <div className="portifolio">
       <StickyBar />
-    </>
+      <div className="sections">
+        <About />
+      </div>
+    </div>
   );
 }
 
