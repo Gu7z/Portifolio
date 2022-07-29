1f9ad322af2b1f8e0eea7c9151f10b025a368722
Gustavo Ferri Alixandre <gustavo.alixandre@luizalabs.com>
2022-07-29 20:14:38 -0300

    refactor: projects style

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -4,11 +4,11 @@ import "./style.css";
 
 const PROJECTS = [
   {
-    name: "Passmonki - Cofre de senhas",
+    name: "Passmonki",
     preview: passmonki,
     url: "https://passmonki.autoseg.com",
     description:
-      "O Passmonki foi o primeiro projeto realmente complexo onde trabalhei, com sua criptografia RSA e um modo de trabalhar com a criação de informações apenas do lado do front-end foi realmente um momento de grande aprendizado.",
+      "O Passmonki é um cofre de senhas e foi o primeiro projeto realmente complexo onde trabalhei, com sua criptografia RSA e um modo de trabalhar com a criação de informações apenas do lado do front-end foi realmente um momento de grande aprendizado.",
   },
   {
     name: "Migueis API",
@@ -18,14 +18,14 @@ const PROJECTS = [
       "A API do Migueis restaurantes foi um projeto em conjunto da UFMS e do Restaurante em si para um prototipo de como seria um sistema de integração de pedidos digitais dentro das dependencias do local.",
   },
   {
-    name: "Slide Puzzle - Game",
+    name: "Slide Puzzle",
     preview: slidePuzzle,
     url: "https://github.com/Gu7z/SlidePuzzle",
     description:
       'O Jogo "Slide Puzzle" é bem antigo e um dos meu jogos favoritos. Como forma de estudo, utilizei ele para entender conceitos de css e também conhecer mais sobre os serviços da Vercel.',
   },
   {
-    name: "Par ou Impar - Game",
+    name: "Par ou Impar",
     preview: parOuImpar,
     url: "https://github.com/Gu7z/par-ou-impar",
     description:
@@ -35,17 +35,17 @@ const PROJECTS = [
 
 const Projects: React.FC = () => {
   return (
-    <>
-      {PROJECTS.map(({ name, preview, url, description }) => (
-        <div className="cards">
+    <section className="cards">
+      <div className="projects">
+        {PROJECTS.map(({ name, preview, url, description }) => (
           <a href={url} className="project" target="_blank" rel="noreferrer">
-            <h3>{name}</h3>
             <img src={preview} alt={`Projeto ${name}`} />
-            <p>{description}</p>
+            <h3>{name}</h3>
+            <p className="color_grey">{description}</p>
           </a>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
+    </section>
   );
 };
 
